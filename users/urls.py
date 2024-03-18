a98655b6bdbfd65c621f6e423820e4b79b2341cc
Ayush-NCI <x23178248@student.ncirl.ie>
2024-03-18 14:02:32 +0000

    password reset functionality added

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('login/', views.userLogin, name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('change_password/', auth_view.PasswordChangeView.as_view(template_name='users/change_password.html'), name='change_password'),
-    path('password_change_done/', auth_view.PasswordChangeDoneView.as_view(template_name='users/change_password_done.html'), name='password_change_done'),
+    path('password_change/done/', auth_view.PasswordChangeDoneView.as_view(template_name='users/change_password_done.html'), name='password_change_done'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password_reset/done', auth_view.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset/complete', auth_view.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
 ]
