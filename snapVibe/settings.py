8bcb62d95ea28b1cd318f6a00f5663d8090f70b5
Ayush-NCI <x23178248@student.ncirl.ie>
2024-03-23 13:14:19 +0000

    static files changes

--- snapVibe/settings.py
+++ snapVibe/settings.py
@@ -133,5 +133,6 @@ LOGOUT_URL='logout'
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
+
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
