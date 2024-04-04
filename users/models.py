f193c3473a278e308ed48805db9898785b53b5d8
EC2 Default User <ec2-user@ip-172-31-44-19.eu-west-1.compute.internal>
2024-04-04 17:00:33 +0000

    fix pylint issues in user module 2.0

--- users/models.py
+++ users/models.py
@@ -1,9 +1,22 @@
+"""
+Module containing models for the users app.
+"""
 from django.db import models
 from django.conf import settings
 # Create your models here.
 class Profile(models.Model):
+    """
+    Model representing user profile information.
+    """
     user =models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo =models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
     
     def __str__(self):
-        return self.user.username
+        """
+        Return a string representation of the profile.
+        """
+        if hasattr(self, 'user') and hasattr(self.user, 'username'):
+            return self.user.username
+        else:
+            return 'Profile'
+        
