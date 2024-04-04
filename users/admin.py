84546e696e11579d38e6e3950275ec6ff93f2368
EC2 Default User <ec2-user@ip-172-31-44-19.eu-west-1.compute.internal>
2024-04-04 16:25:11 +0000

    resolved pylint issues for user module (admin and apps)

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,7 @@
+"""
+This module contains the admin views and functionalities.
+"""
 from django.contrib import admin
 from .models import Profile
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register
--- users/apps.py
+++ users/apps.py
@@ -1,6 +1,11 @@
+"""
+Module containing application configuration for the 'users' app.
+"""
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
+    """
+    Application configuration class for the 'users' app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
