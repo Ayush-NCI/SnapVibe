c5c96518207a764e42eb7153ba92fd1006136b1c
EC2 Default User <ec2-user@ip-172-31-44-19.eu-west-1.compute.internal>
2024-04-04 16:53:17 +0000

    fixed pylint error for users module

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,4 @@ This module contains the admin views and functionalities.
 from django.contrib import admin
 from .models import Profile
 # Register your models here.
-admin.site.register
+admin.site.register(Profile)
--- users/forms.py
+++ users/forms.py
@@ -1,32 +1,70 @@
+"""
+Module containing form definitions for the users app.
+"""
 from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 
 
 class LoginForm(forms.Form):
+    """
+    Form for login the user.
+    """
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
     
 class UserRegistrationForm(forms.ModelForm): 
-    
+    """
+    Form for creating user information.
+    """
     password=forms.CharField(label='Password', widget=forms.PasswordInput)
     password2=forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-    
     class Meta:
+        """
+        Metadata for the UserRegistrationForm.
+
+        Specifies the associated User model and fields for the form.
+        """
         model=User
         fields={'username','email','first_name', 'last_name'}
         
     def check_password(self):
+        """
+        Method to check if the entered passwords match.
+
+        Returns:
+            str: The confirmed password.
+
+        Raises:
+            forms.ValidationError: If the passwords do not match.
+        """
         if self.cleaned_data['password']!=self.cleaned_data['password2']:
             raise forms.ValidationError('Passwords do not match')
         return self.cleaned_data['password2']   
         
 class UserEditForm(forms.ModelForm):
+    """
+    Form for editing user information.
+    """
     class Meta:
+        """
+        Metadata for the UserEditForm.
+
+        Specifies the associated User model and fields for the form.
+        """
         model=User
         fields=('first_name','last_name','email')
         
 class ProfileEditForm(forms.ModelForm):
+    """
+    Form for creating/editing user profile information.
+    """
     class Meta:
+        """
+        Metadata for the ProfileEditForm.
+
+        Specifies the associated Profile model and fields for the form.
+        """
         model=Profile
-        fields=('photo',)        
+        fields=('photo',) 
+        
