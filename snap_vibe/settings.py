a936e971d1ed534e17c2aa5df003c28b673a1ceb
EC2 Default User <ec2-user@ip-172-31-18-251.eu-west-1.compute.internal>
2024-04-07 21:12:01 +0000

    python library correction

--- snap_vibe/settings.py
+++ snap_vibe/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-from decouple import config # pylint: disable=no-name-in-module
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
