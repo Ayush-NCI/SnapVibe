8948a344a00008e4d785035ddce0fe99fd43b6d1
EC2 Default User <ec2-user@ip-172-31-18-251.eu-west-1.compute.internal>
2024-04-07 21:04:17 +0000

    disabled no-name-module

--- snap_vibe/settings.py
+++ snap_vibe/settings.py
@@ -10,10 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import json
-import logging
 from pathlib import Path
-from decouple import config
+from decouple import config # pylint: disable=no-name-in-module
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
